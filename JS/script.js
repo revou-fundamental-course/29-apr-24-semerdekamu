c5cfd8031ec3f8fd4117a2895c51108094df51c7
Adi Prayogi <adipray999@gamil.com>
2024-05-09 20:41:00 +0700

    initial config

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,51 @@
 //Ini javascript
 
+// Validate Function
 function validation() {
-    console.log('Good');
+    let username = document.getElementById('form-username').value;
+    console.log(username);
+    if (username == '' || username == null) {
+        document.getElementById('form-username').style.border = '1px solid red';
+        alert('Inputan Anda Kosong, Periksa Kembali')
+    } else {
+        alert('Welcome' + username)
+        console.log(username);
+    }
 }
 
-document.getElementById('home-button').addEventListener('click', () => validation())
+
+// Section Slide
+let indexSlide = 1; // [0, 1, 2]
+showSlide(1);
+
+function nextSlide(n) {
+    console.log('nextSlide: ' + n)
+    showSlide(indexSlide += n);
+    
+}
+
+// Slide Logic atau trigerr
+function showSlide(index) {
+    let listImage = document.getElementsByClassName ('banner-item');
+    console.log(listImage);
+    console.log('Banyaknya image: ' + listImage.length);
+
+    // Reset SlideIndex
+    if (index > listImage.length) indexSlide = 1;
+
+    // hide all images
+    let i = 0;
+    while (i < listImage.length) {
+        listImage[i].style.display = 'none';
+        i++;
+    }
+
+    // Show only one image
+    listImage[indexSlide - 1].style.display = 'block';
+
+    console.log('showSlide: ' + index);
+}
+
+setInterval(() => nextSlide(1), 2000);
+
+document.getElementById('send-button').addEventListener('click', () => nextSlide(1))
